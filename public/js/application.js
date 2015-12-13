8c6abe90906867f5d033ef7e20af2b7b1e817a7e
Mark Janzer <email": null>
2015-12-12 17:42:32 -0800

    Complete rhythm creation :squirrel:

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,53 @@
 $(document).ready(function() {
+  beginMeasureCreation();
+  beginTextAddition();
+  addValueToMeasure();
+  retryRhythmCreation();
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
+var beginMeasureCreation = function(){
+  $(document).on("click", ".begin-measure-creation", function(event){
+    event.preventDefault();
+    $(".begin-measure-creation").hide();
+    $(".rhythm-creation-div").show();
+  })
+}
+
+var beginTextAddition = function(){
+  $(document).on("click", ".create-rhythm", function(event){
+    event.preventDefault();
+    $(".rhythm-creation-div").hide();
+    $(".add-text-div").show();
+  })
+}
+
+var addValueToMeasure = function(){
+  $(document).on("click", ".duration-button", function(event){
+    event.preventDefault();
+    var duration = $(this).val();
+    $(".measure-creation-values").append(duration);
+    $(".measure-creation-values").append(" ");
+
+    if (duration.split(" ").length > 1) {
+      $(".measure-creation-total").text("1")
+    } else {
+      var previous_sum = $(".measure-creation-total").text()
+      var current_sum = eval(duration) + eval(previous_sum)
+      $(".measure-creation-total").text(current_sum.toString())
+    }
+  })
+}
+
+var retryRhythmCreation = function(){
+  $(document).on("click", ".retry-rhythm-creation", function(event){
+    event.preventDefault();
+    $(".measure-creation-values").text("");
+    $(".measure-creation-total").text("0");
+  })
+}
+
