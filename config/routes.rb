3a232cc26ecf8d0b85341342e2c36c286e6d05d2
markjanzer <mark.janzer@gmail.com>
2016-02-19 14:15:37 -0800

    Create new song form with form_for

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
     resources :songs
+    resources :artists
+    resources :albums
 
   # Example resource route with options:
   #   resources :products do
