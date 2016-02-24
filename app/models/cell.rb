61ed3cde0827c682c8a37d81775b8a281a6c1ad7
markjanzer <mark.janzer@gmail.com>
2016-02-24 13:09:22 -0800

    Begin song show through partials. Currently broken

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,2 +1,19 @@
 class Cell < ActiveRecord::Base
+  # Makes html cells easier (maybe)
+  def html(attribute)
+    case attribute
+    when "stressed"
+      if this.stressed
+        return "stressed"
+      else
+        return ""
+      end
+    when "end_rhyme"
+      if this.end_rhyme
+        return "end-rhyme"
+      else
+        return ""
+      end
+    end
+  end
 end
--- app/models/measure.rb
+++ app/models/measure.rb
@@ -1,3 +1,8 @@
 class Measure < ActiveRecord::Base
   has_many :cells
+
+  def ordered_cells
+    cells = this.cells.sort_by { |cell| cell.measure_number }
+    return cells
+  end
 end
