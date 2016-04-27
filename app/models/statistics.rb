53dc75d69ba86f2167bbb040f4080aa83ed21814
markjanzer <mark.janzer@gmail.com>
2016-04-26 20:10:35 -0700

    Add percentages to statistics. Change to 'measures WITH stress on b WITH stress on beat one'

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -1,7 +1,7 @@
 module Statistics
 
   def percent_of_rhyming_syllables
-    self.cells.where.not(rhyme: 0).count/self.cells.where.not(content: nil).count.to_f
+    (self.cells.where.not(rhyme: 0).count/self.cells.where.not(content: nil).count.to_f) * 100
   end
 
   def percent_of_measures_with_stress_on_beat_one
@@ -10,7 +10,7 @@ module Statistics
         cell.note_beginning == 1 && cell.stressed == true
       end
     end
-    stressed_one.count / self.measures.count.to_f
+    (stressed_one.count / self.measures.count.to_f) * 100
   end
 
   def measures_with_end_rhyme
@@ -27,30 +27,34 @@ module Statistics
 
   def placement_of_stressed_syllables
     hash = {}
+    total_stressed_syllables = 0
     self.cells.where(stressed: true).each do |cell|
-      # edge case of first note
+      # refactor edge case of first note
+      total_stressed_syllables += 1
       beginning = ((cell.note_beginning - 1)/960.0).to_s.to_r
       hash[beginning] ||= 0
       hash[beginning] += 1
     end
     sorted_array = []
     hash.keys.sort.each do |key|
-      sorted_array.push({onset: key, quantity: hash[key]})
+      sorted_array.push({onset: key, count: hash[key], percent: ((hash[key] * 100.0)/total_stressed_syllables).round(2)})
     end
     sorted_array
   end
 
   def placement_of_end_rhymes
     hash = {}
+    total_end_rhymes = 0
     self.cells.where(end_rhyme: true).each do |cell|
       # edge case of first note
+      total_end_rhymes += 1
       beginning = ((cell.note_beginning - 1)/960.0).to_s.to_r
       hash[beginning] ||= 0
       hash[beginning] += 1
     end
     sorted_array = []
     hash.keys.sort.each do |key|
-      sorted_array.push({onset: key, quantity: hash[key]})
+      sorted_array.push({onset: key, count: hash[key], percent: ((hash[key] * 100.0)/total_end_rhymes).round(2)})
     end
     sorted_array
   end
