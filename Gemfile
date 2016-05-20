5a6a50067c36b80ea0e40b2f2c75910922c249ea
markjanzer <mark.janzer@gmail.com>
2016-05-19 18:30:24 -0700

    Add factory girl. Add spec/models

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :development, :test do
   gem 'byebug'
   gem 'rspec-rails', '~> 3.4'
   gem 'capybara'
+  gem 'factory_girl'
 end
 
 group :development do
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+# describe User, '#username' do
+#   it 'returns the username' do
+#     # setup
+#     user = build(:user, username: 'Swoo')
+
+#     # excercise and verify
+#     expect(user.username).to eq 'Swoo'
+#   end
+# end
